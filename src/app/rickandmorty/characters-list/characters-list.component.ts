b02ac51fe493d2a45fbc2088c45cf0675059047a
adarshdsw <adarshsingh@cdnsol.com>
2024-02-15 01:05:17 +0530

    addedd bootstarp module to create table to show data and added a bootstrap table in list component and added data dynamically with detail page link in table

--- src/app/rickandmorty/characters-list/characters-list.component.ts
+++ src/app/rickandmorty/characters-list/characters-list.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-characters-list',
   standalone: true,
-  imports: [HttpClientModule,],
+  imports: [HttpClientModule,CommonModule],
   templateUrl: './characters-list.component.html',
   styleUrl: './characters-list.component.css'
 })
 export class CharactersListComponent {
 
   characters: any = [];
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.getCharacters();
@@ -34,6 +36,11 @@ export class CharactersListComponent {
         console.log('error ', error);
       }
     });
+  }
+
+  // Navigate to character detail page
+  characterDetail(id:any){
+    this.router.navigate(['characters/'+id]);
   }
 
 }
