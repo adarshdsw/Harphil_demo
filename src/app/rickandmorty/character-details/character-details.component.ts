481ea04583cff3ce8dc61629eb1dc17d5979d295
adarshdsw <adarshsingh@cdnsol.com>
2024-02-15 01:28:32 +0530

    created detail page and fetch details from API and show in detail components

--- src/app/rickandmorty/character-details/character-details.component.ts
+++ src/app/rickandmorty/character-details/character-details.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-character-details',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './character-details.component.html',
   styleUrl: './character-details.component.css'
 })
 export class CharacterDetailsComponent {
+
+  characterId: any;
+  characterDetail: any;
+
+  constructor(private activeRouter: ActivatedRoute, private http: HttpClient) {
+    // Fetch Id from url 
+    this.activeRouter.params.subscribe((param: any) => {
+      this.characterId = param['id'];
+    })
+    console.log('this.characterId ', this.characterId);
+  }
+
+  ngOnInit(): void {
+    this.getCharacterDetail(this.characterId);
+  }
+
+  // Fetch specific character detail by id
+  getCharacterDetail(id: any) {
+    this.http.get('https://rickandmortyapi.com/api/character/' + id).subscribe({
+      next: (response: any) => {
+        console.log('response ', response);
+        if (response) {
+          this.characterDetail = response;
+        }
+      },
+      error: (error) => {
+        console.log('error ', error);
+      }
+    })
+  }
 
 }
